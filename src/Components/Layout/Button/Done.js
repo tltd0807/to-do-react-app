7c291f0c68574156c2b4a9988f452583f7a2b574
Dat <todat2001@gmail.com>
2022-09-12 21:54:22 +0700

    fix tick div to input todo

--- src/Components/Layout/Button/Done.js
+++ src/Components/Layout/Button/Done.js
@@ -1,8 +1,8 @@
 import classes from "./Done.module.css";
 import Icon from "../../../asset/images/icon-check.svg";
-const Done = () => {
+const Done = (props) => {
   return (
-    <div className={classes.done}>
+    <div className={`${classes.done} ${props.className}`}>
       <img src={Icon} alt="icon-check" />
     </div>
   );
