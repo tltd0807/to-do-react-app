8da8bfb88aaa19f1cb41bb95849f6b1d92969e03
Dat <todat2001@gmail.com>
2022-09-20 15:37:15 +0700

    fix the footer always have 1 filter active

--- src/Components/ToDoBody/ToDoFilter/ToDoFilter.js
+++ src/Components/ToDoBody/ToDoFilter/ToDoFilter.js
@@ -5,9 +5,24 @@ import Card from "../../Layout/Card/Card";
 const ToDoFilter = (props) => {
   return (
     <Card className={classes.container}>
-      <div onClick={() => props.onFilter(1)}>All</div>
-      <div onClick={() => props.onFilter(2)}>Active</div>
-      <div onClick={() => props.onFilter(3)}>Completed</div>
+      <div
+        className={classes[props.filter.all ? "active" : ""]}
+        onClick={() => props.onFilter(1)}
+      >
+        All
+      </div>
+      <div
+        className={classes[props.filter.active ? "active" : ""]}
+        onClick={() => props.onFilter(2)}
+      >
+        Active
+      </div>
+      <div
+        className={classes[props.filter.completed ? "active" : ""]}
+        onClick={() => props.onFilter(3)}
+      >
+        Completed
+      </div>
     </Card>
   );
 };
